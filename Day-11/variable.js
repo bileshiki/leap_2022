8b243669b18fa741e7d6709d8a20c18f4f0f3fd7
Morning <morning@Admins-iMac-4.local>
2022-11-28 10:56:57 +0800

    added JS

---
+++ Day-11/variable.js
@@ -0,0 +1,22 @@
+
+ let firstName = "Bilguun";
+ console.log(typeof(firstName));
+ console.log(firstName);
+
+ let age = 27;
+ console.log(typeof(age));
+ console.log(age);
+ 
+ let fname = true;
+console.log(typeof(fname));
+console.log(fname);
+
+let a = null
+console.log(typeof(a));
+console.log(a);
+
+let b = undefined;
+console.log(typeof(b));
+
+let w 
+console.log(w);
