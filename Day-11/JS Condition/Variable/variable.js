49ac32c32c40fdde57c1fabb76611d35fcdf7989
Morning <morning@Admins-iMac-4.local>
2022-11-30 12:03:59 +0800

    added variasbles js

---
+++ Day-11/JS Condition/Variable/variable.js
@@ -0,0 +1,20 @@
+//1-r dasgal
+
+let a = 85;
+let b = 75;
+let c = 96;
+let d = 69;
+let e = 80;
+let f = a + c;
+
+console.log(f);
+
+// 2-r dasgal
+
+let a1 = 3;
+let b1 = 7;
+let c1 = 2;
+let d1 = 4;
+let e1 = a1 * c1 * d1;
+
+console.log(e1);
