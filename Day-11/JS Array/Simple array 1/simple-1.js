14bcde6ae141270107f692b024da87e79c0afb2b
Morning <morning@Admins-iMac-4.local>
2022-12-05 12:32:43 +0800

    added number digits

---
+++ Day-11/JS Array/Simple array 1/simple-1.js
@@ -0,0 +1,19 @@
+// Arrey - ын хувьсагчийг itCompanies гэж зарлаж, Facebook, Google,
+//  Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу. 
+
+// console.log() ашиглан array хэвлэх
+const itCompanies = ["Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"]
+console.log(itCompanies);
+
+// Array дахь компаниудын length хэвлэх
+for (let i = 0; i < itCompanies.length; i++) {
+    console.log(itCompanies[i].length);
+    
+}
+
+// Эхний компани, дунд болон сүүлчийн компанийг хэвлэх
+for(let i = 0; i < itCompanies.length; i++)
+// Компани бүрийг хэвлэх
+// Компанийн нэр тус бүрийг нэг нэгээр нь том үсгээр сольж хэвлэж гарга
+// Array - г өгүүлбэр болгон хэвлэ: Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг мэдээллийн технологийн томоохон компаниуд.
+
