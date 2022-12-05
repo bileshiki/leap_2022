c63e0f4cffd1004a634393d5bb593f1f689f20df
Coder <Coder@Ges-MacBook-Air.local>
2022-12-05 21:40:10 +0800

    added JS array

--- Day-11/JS Array/Simple array 1/simple-1.js
+++ Day-11/JS Array/Simple array 1/simple-1.js
@@ -2,7 +2,7 @@
 //  Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу. 
 
 // console.log() ашиглан array хэвлэх
-const itCompanies = ["Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"]
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 console.log(itCompanies);
 
 // Array дахь компаниудын length хэвлэх
@@ -12,8 +12,31 @@ for (let i = 0; i < itCompanies.length; i++) {
 }
 
 // Эхний компани, дунд болон сүүлчийн компанийг хэвлэх
-for(let i = 0; i < itCompanies.length; i++)
-// Компани бүрийг хэвлэх
-// Компанийн нэр тус бүрийг нэг нэгээр нь том үсгээр сольж хэвлэж гарга
-// Array - г өгүүлбэр болгон хэвлэ: Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг мэдээллийн технологийн томоохон компаниуд.
+// let i = 0; 
+// while (i < itCompanies.leng) {
+//     console.log(itCompanies[i][0]);
+// }
+// console.log(itCompanies[itCompanies.length - 1], "First element");
+// for(let i = 1; i < itCompanies.length; i++) {
+//     for(let j = -1; j < itCompanies.length -1; j++) {
+//         itCompanies = i == j;
+//         console.log(itCompanies[i] [j]);
+//     }
+//             console.log(itCompanies[i] [j]);
+//         }
+//         console.log(itCompanies);
 
+// Компани бүрийг хэвлэх
+let i = 0 
+let j = "";
+while (i < itCompanies.length) {
+    j += itCompanies[i] + " ";
+    i++;
+}
+console.log(j);
+// Компанийн нэр тус бүрийг нэг нэгээр нь том үсгээр сольж хэвлэж гарга
+
+// Array - г өгүүлбэр болгон хэвлэ: Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon 
+// зэрэг мэдээллийн технологийн томоохон компаниуд.
+let uguulber = `${itCompanies}\nзэрэг мэдээллийн технологийн томоохон компаниуд.`;
+console.log(uguulber);
