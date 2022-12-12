07330dc45535ded4c878b6ef486186d89c8d5b87
Coder <Coder@Ges-MacBook-Air.local>
2022-12-12 20:13:33 +0800

    added DOM

---
+++ Day-11/DOM/dom.js
@@ -0,0 +1,1 @@
+document.getElementById("helloText").innerHTML = "Hello World!";
---
+++ Day-11/DOM/dummy.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("dummy").innerHTML = "dudu"; //desktop deer haragdahgui bn. Bagshaas asuuh?
+document.getElementById("demo").innerHTML = "Date : " + Date();
+// document.querySelector(".dummy").innerHTML = "dudu"; ingej desktop deer haragdaj bn.
