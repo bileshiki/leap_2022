2d98930fd9c033e46fce626bc03080c041eb33e6
IceBear <IceBear@Ges-MacBook-Air.local>
2022-12-20 15:59:51 +0800

    update pointp board

--- Day-11/DOM/aimag.js
+++ Day-11/DOM/aimag.js
@@ -1,12 +1,23 @@
 // 1. Хамгийн эхний li элементийг аван утгыг console.log дээр хэвлэх.
-function myFunction() {
-    let firstLi = Document.querySelecterAll("li")[1];
-    let list = [];
-    for (let i = 0; i < firstLi.children.length; i++) {
-        let element = firstLi.children[i];
-        if(element.tagName === "LI")
-        list.push(element);
-    }
-    return list;
-}
-myFunction();
+let ulEl = document.getElementsByTagName("ul");
+
+console.log(ulEl);
+console.log(ulEl[0].firstElementChild);
+
+// 2.Хамгийн сүүлийн li элементийг аван утгыг console.log дээр хэвлэх.
+console.log(ulEl[0].lastElementChild);
+
+// 3. Сүхбаатар аймгийг элементийн id-аар аван console.log дээр хэвлэх
+console.log(document.getElementById("aimag-315"));
+
+// 3. Тухайн жагсаалтын элементүүдэд загвар оруулах (текстийн фонт, хэмжээ, текстийн өнгө, хүрээ, padding, margin).
+console.log(document.getElementById("aimag-101").style.color = "green");
+console.log(document.getElementById("aimag-101").style.border = "1px solid green");
+console.log(document.getElementById("aimag-101").style.padding = "20px");
+console.log(document.getElementById("aimag-101").style.margin = "10px");
+console.log(document.getElementById("aimag-101").style.borderRadius = "20px");
+console.log(document.getElementById("aimag-101").style.fontSize = "24px");
+
+// 4. Аймгуудыг бүс, бүсээр ялган өөр, өөр background color  өгөх  
+// (1 - Хангайн бүс, 2 - Баруун бүс, 3 - Зүүн бүс, 4 - Төвийн бүс, 5 - Улаанбаатар )
+//  Жишээ нь id = aimag-321 ийм байгаа 3 нь бүсийн дугаар, 21 нь аймгийн дугаар юм.
--- Project/point-board.js
+++ Project/point-board.js
@@ -1,7 +1,47 @@
-let firstName = document.getElementsByClassName ("firstName").value;
-console.log(firstName);
-let lastName = document.getElementsByClassName("lastName").value;
-console.log(lastName);
-
+let firstName = document.getElementsByClassName ("firstName");
+let lastName = document.getElementsByClassName("lastName");
+let gender= document.getElementsByClassName("gender");
+let point = document.getElementsByClassName("point");
+let sBtn = document.getElementById("btn");
+let lists = document.getElementsByClassName("lists");
 let fivePoint = document.getElementsByClassName("fivePoint");
-fivePoint.addEventListener("click",);
+let del = document.getElementsByClassName("del");
+let arrList = [];
+
+function submitBtn(event) {
+    event.preventDefault();
+    let gen;
+        if (gender.value == "1") {
+             gen = "Эрэгтэй";
+        } else if (gender.value == "2") {
+             gen = "Эмэгтэй";
+        }
+    let listItem = {firstName: firstName.value,
+                    lastName: lastName.value,
+                    gender: gen,
+                    point: point.value,
+                    }
+    arrList.push(listItem);
+    console.log(arrList);
+    reload();
+}
+
+sBtn.EventListener("click", submitBtn);
+function reload() {
+    let row = ""
+    for (let i = 0; i < arrList.length; i++) {
+        row +=  ` 
+        <div>
+            <div>
+                ${arrList[i].firstName}
+                ${arrList[i].lastName}
+                ${arrList[i].gender}
+                <div class="point">
+                ${arrList[i].point}
+                </div>
+            </div>
+            <a class="addPoint"Add 5 point/>
+        </div>`
+    }  
+    lists.innerHTML = row
+}
