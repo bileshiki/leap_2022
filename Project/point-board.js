b649a4f90f8eae52936e766116d0e88834af9e0e
Morning <morning@Admins-iMac-4.local>
2022-12-20 12:41:14 +0800

    update point board

---
+++ Project/point-board.js
@@ -0,0 +1,7 @@
+let firstName = document.getElementsByClassName ("firstName").value;
+console.log(firstName);
+let lastName = document.getElementsByClassName("lastName").value;
+console.log(lastName);
+
+let fivePoint = document.getElementsByClassName("fivePoint");
+fivePoint.addEventListener("click",);
